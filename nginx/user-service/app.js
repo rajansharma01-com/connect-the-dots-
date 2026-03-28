8ca5394b58a919236d94f680bd199691308ce708
Rajan Sharma <rajansharma2309@gmail.com>
2026-03-28 21:44:28 +0530

    Initialize user service with Express server

---
+++ nginx/user-service/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('User Service Running');
+});
+
+app.listen(3001, () => {
+  console.log('User Service on 3001');
+});
